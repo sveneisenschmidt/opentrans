0488fa389616d4c24a502db3d708714bf1efeea1
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2013-10-21 15:19:23 +0200

    order document node tests

---
+++ tests/SE/Component/OpenTrans/Tests/Node/Order/ControlInfoNodeTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the OpenTrans php library
+ *
+ * (c) Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SE\Component\OpenTrans\Tests\Node\Order;
+
+/**
+ *
+ * @package SE\Component\OpenTrans\Tests
+ * @author Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
+ */
+class ControlInfoNodeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @test
+     */
+    public function CanBeInitialized()
+    {
+        $loader = new \SE\Component\OpenTrans\NodeLoader();
+        $node1 = new \SE\Component\OpenTrans\Node\Order\ControlInfoNode();
+        $node2 = $loader->getInstance(\SE\Component\OpenTrans\NodeLoader::NODE_ORDER_CONTROLINFO);
+
+        $this->assertInstanceOf('\SE\Component\OpenTrans\Node\Order\ControlInfoNode', $node1);
+        $this->assertInstanceOf('\SE\Component\OpenTrans\Node\Order\ControlInfoNode', $node2);
+
+        $this->assertEquals(get_class($node1), get_class($node2));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function SetterAndGetterTest()
+    {
+        $node = new \SE\Component\OpenTrans\Node\Order\ControlInfoNode();
+
+        $node->setGeneratorInfo($generatorInfo = sha1(microtime(true)));
+        $this->assertEquals($generatorInfo, $node->getGeneratorInfo());
+
+        $date = new \DateTime(sprintf('@%s', rand(1, time())));
+        $node->setGenerationDate($date);
+        $this->assertSame($date, $node->getGenerationDate());
+    }
+}
---
+++ tests/SE/Component/OpenTrans/Tests/Node/Order/DocumentNodeTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of the OpenTrans php library
+ *
+ * (c) Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SE\Component\OpenTrans\Tests\Node\Order;
+
+/**
+ *
+ * @package SE\Component\OpenTrans\Tests
+ * @author Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
+ */
+class DocumentNodeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @test
+     */
+    public function CanBeInitialized()
+    {
+        $loader = new \SE\Component\OpenTrans\NodeLoader();
+        $node1 = new \SE\Component\OpenTrans\Node\Order\DocumentNode();
+        $node2 = $loader->getInstance(\SE\Component\OpenTrans\NodeLoader::NODE_ORDER_DOCUMENT);
+
+        $this->assertInstanceOf('\SE\Component\OpenTrans\Node\Order\DocumentNode', $node1);
+        $this->assertInstanceOf('\SE\Component\OpenTrans\Node\Order\DocumentNode', $node2);
+
+        $this->assertEquals(get_class($node1), get_class($node2));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function SetterAndGetterTest()
+    {
+        $node = new \SE\Component\OpenTrans\Node\Order\DocumentNode();
+
+        $header = new \SE\Component\OpenTrans\Node\Order\HeaderNode();
+        $node->setHeader($header);
+        $this->assertSame($header, $node->getHeader());
+
+        $summary = new \SE\Component\OpenTrans\Node\Order\SummaryNode();
+        $node->setSummary($summary);
+        $this->assertSame($summary, $node->getSummary());
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function SetAndGetItems()
+    {
+        $node = new \SE\Component\OpenTrans\Node\Order\DocumentNode();
+
+        $item1 = new \SE\Component\OpenTrans\Node\Order\ItemNode();
+        $item2 = new \SE\Component\OpenTrans\Node\Order\ItemNode();
+
+        $this->assertEmpty($node->getItems());
+
+        $node->setItems([$item1, $item2]);
+        $this->assertCount(2, $node->getItems());
+        $this->assertEquals($node->getItems(), [$item1, $item2]);
+
+        $node->addItem($item2);
+        $this->assertCount(3, $node->getItems());
+        $this->assertEquals($node->getItems(), [$item1, $item2, $item2]);
+    }
+}
